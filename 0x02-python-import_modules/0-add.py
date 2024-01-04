fd449007366948c8eaaf6127644b962d62a286d9
meron6 <khozameron5@gmail.com>
2024-01-04 08:49:34 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-# Import the add function from add_0.py
-from add_0 import add
+de add(a, b):
+"""My addition function
 
-# Assign the value 1 to a variable called a
-a = 1
+Arg:
+a: first integer
+b: second integer
 
-# Assign the value 2 to a variable called b
-b = 2
-
-# Print the result of the addition using string format
-print("{} + {} = {}".format(a, b, add(a, b)))
+Return: 
+The return value. a + b
+"""
+return (a + b}
