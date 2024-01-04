78575c1007f03bddc34a062c5f9fe78f3b726abe
meron6 <khozameron5@gmail.com>
2024-01-04 11:00:00 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    from hidden_4 import *
-    allf = dir()
-    for i in range(0, len(allf)):
-        if allf[i] != "__":
-            print("{:s}".format(allf[i]))
+    args = sys.argv[1:]
+    total = sum(map(int, args))
+    print(total)
