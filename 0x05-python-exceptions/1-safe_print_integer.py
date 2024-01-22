3cd58afe594b5edf9286792f3aee31f3573bfa59
meron6 <khozameron5@gmail.com>
2024-01-22 14:13:46 -0800

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
+# 1-safe_print_integer.py
+
 
 def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
     try:
         print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
-
-# Test cases
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
