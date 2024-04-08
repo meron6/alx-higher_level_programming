a9eb05d28046469a5e3b12fef3f6eecd30d9935f
meron6 <khozameron5@gmail.com>
2024-04-08 12:44:03 -0700

    0-javascript_is_amazing.js

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+// 0-javascript_is_amazing.js
+
+// Create a constant variable called myVar with the value "JavaScript is amazing"
+const myVar = "JavaScript is amazing";
+
+// Use console.log(...) to print the output
+console.log(myVar);
