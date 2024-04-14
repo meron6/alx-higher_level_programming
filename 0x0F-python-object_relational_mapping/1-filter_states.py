1a2377089f36dd6e6b1940c74306f98105c12668
meron6 <khozameron5@gmail.com>
2024-04-14 08:22:47 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,38 +1,14 @@
 #!/usr/bin/python3
-"""
-lists states that starts with capital letter "N"
-"""
-import MySQLdb
+"""Module that lists all states from the hbtn_0e_0_usa database."""
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        try:
-            host = "localhost"
-            port = 3306
-            usr = sys.argv[1]
-            passwd = sys.argv[2]
-            db = sys.argv[3]
+    # Get MySQL credentials from command-line arguments
+    # Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
 
-            sql_query = "SELECT * FROM states ORDER BY id ASC"
-
-            con = MySQLdb.connect(host=host, port=port,
-                                  user=usr, passwd=passwd, db=db)
-
-            cur = con.cursor()
-
-            cur.execute(sql_query)
-
-            nstates = cur.fetchall()
-
-            for nstate in nstates:
-                if nstate[1][0] == 'N':
-                    print("{}".format(nstate))
-
-        except:
-            print("error reading mysql query")
-
-        finally:
-
-            cur.close()
-            con.close()
+    # Execute the SQL query to retrieve all states sorted by id
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
